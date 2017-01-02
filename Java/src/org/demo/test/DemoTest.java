4e25a33de60edd4fcc8fd63545f2df7608335637
kartik-netsmartz <kartik.raina@netsmartz.com>
2017-01-02 15:01:38 +0530

    Update DemoTest.java

--- Java/src/org/demo/test/DemoTest.java
+++ Java/src/org/demo/test/DemoTest.java
@@ -7,6 +7,6 @@ public class DemoTest {
     public static void main(String[] args){
 
         System.out.println("TEST TEST TEST Changes done for Test Branch");
-        System.out.println("Some more chnages done my the owner of the code.")
+        System.out.println("Some more changes done my the owner of the code.");
     }
 }
