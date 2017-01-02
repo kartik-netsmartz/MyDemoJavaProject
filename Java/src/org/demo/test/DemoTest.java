1cb5f5da7c6a5809ac03fb06aad262d1d19ca640
DESKTOP-521QHCN\Developer <kartik.raina@netsmartz.com>
2017-01-02 02:16:10 -0700

    DemoProjetcCheckin

---
+++ Java/src/org/demo/test/DemoTest.java
@@ -0,0 +1,11 @@
+package org.demo.test;
+
+/**
+ * Created by Developer on 1/2/2017.
+ */
+public class DemoTest {
+    public static void main(String[] args){
+
+        System.out.println("TEST TEST TEST");
+    }
+}
